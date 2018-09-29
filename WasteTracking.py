e0360516df5c943e46aa70f759b30f74e5bfe8db
joshuaw-rae <38540422+joshuaw-rae@users.noreply.github.com>
2018-09-29 18:13:28 -0400

    Gspread code set up, starting on tkinter

--- WasteTracking.py
+++ WasteTracking.py
@@ -1,3 +1,27 @@
-print("This is the code for the waste tracking program")
-print("Here is the second line of code")
-print("Here is line 3")
+import gspread
+from tkinter import *
+from oauth2client.service_account import ServiceAccountCredentials
+
+# gspread code
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json',scope)
+client = gspread.authorize(creds)
+
+sheet = client.open('WasteTracking').sheet1
+
+date = input()
+##
+
+# tkinter code
+root = Tk()  # main window
+
+root.mainloop()  # display window on screen
+##
+
+
+
+#row = ["this","is","awesome!"]
+
+#index = 2
+
+#sheet.insert_row(row,index)

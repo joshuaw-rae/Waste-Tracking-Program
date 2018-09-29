043afec3567fc32a7f063ec2b60779e03e1c2e02
joshuaw-rae <38540422+joshuaw-rae@users.noreply.github.com>
2018-09-29 18:41:11 -0400

    Basic tkinter

--- WasteTracking.py
+++ WasteTracking.py
@@ -8,19 +8,49 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json',sc
 client = gspread.authorize(creds)
 
 sheet = client.open('WasteTracking').sheet1
-
-date = input()
 ##
 
 # tkinter code
 root = Tk()  # main window
 
+label1 = Label(root,text="Date")
+label2 = Label(root,text="Item")
+label3 = Label(root,text="Source")
+
+entry1 = Entry(root,bg="white",fg="blue")
+entry2 = Entry(root,bg="white",fg="blue")
+entry3 = Entry(root,bg="white",fg="blue")
+
+button1=Button(root,text="Submit data",command=submitdata())
+
+label1.grid(row=0)
+entry1.grid(row=0,column=1)
+
+label2.grid(row=2)
+entry2.grid(row=2,column=1)
+
+label3.grid(row=4)
+entry3.grid(row=4,column=1)
+
+button1.grid(row=6,column=1)
+
+date = entry1.get()
+item = entry2.get()
+source = entry3.get()
+
+print(date)
+print(item)
+print(source)
 root.mainloop()  # display window on screen
 ##
 
+def submitdata():
+    print("boise")
 
 
-#row = ["this","is","awesome!"]
+
+submitdata()
+#row = [date,item,source]
 
 #index = 2
 
